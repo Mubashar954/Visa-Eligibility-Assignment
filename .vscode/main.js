9ea037c657f9363db3220322a46934621ef13aa7
Bashr_Momen <59894011+Mubashar954@users.noreply.github.com>
2020-12-24 23:43:39 +0500

    creating doc

---
+++ .vscode/main.js
@@ -0,0 +1,5 @@
+/// <reference path="c:/Users/Bashr_Momen/.vscode/extensions/nur.script-0.0.7/global.d.ts" />
+// API - https://code.visualstudio.com/api/references/vscode-api 
+// reload window after edit...
+
+window.showInformationMessage('Hello World!')
