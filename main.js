977679ba89096f11135d08c808f71ee51f310af4
Bashr_Momen <59894011+Mubashar954@users.noreply.github.com>
2020-12-25 00:07:05 +0500

    completed

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log("Hello Momen")
+
+var age
+var Senior = false;
+var Young = false;
+var child = false;
+
+age =21;
+if(age>=60){
+    console.log("You are Senior")
+    console.log("You are elegible to apply for visa")
+    Senior = true;
+}
+ else if(age>=20){
+    console.log("You are Young")
+    console.log("You are elegible to apply for visa")
+    Young = true;
+}
+ else if(age>=1){
+    console.log("You are Child")
+    console.log("You are not elegible to apply for visa")
+    child = true;
+}
